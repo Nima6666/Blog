c650f6297eb4d5e185c83a20dcfa223784911061
nima <me@ubuntu.com>
2024-01-08 09:09:38 +0545

    some insights

--- blog-server/model/admin.js
+++ blog-server/model/admin.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const adminSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: true },
 });
 
 module.exports = mongoose.model("theAdmin", adminSchema);
